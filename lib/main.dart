3b2114aea0ff1331acc19d7ad44c8f1cd4382889
Max Marschhauser <max.marschhauser@gmail.com>
2023-06-28 11:08:19 +0200

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+import 'package:chat_app/screens/auth.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
@@ -16,7 +24,7 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: const Text('chat app'),
+      home: const AuthScreen(),
     );
   }
 }
